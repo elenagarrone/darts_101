b186224c1c4fe8ce80fdc3dc7d13400ad9b31c06
elena <elena.15@hotmail.it>
2014-11-10 16:07:28 +0000

    dart should not be thrown when initialized, turn should hold darts

--- spec/turnSpec.js
+++ spec/turnSpec.js
@@ -2,10 +2,16 @@ describe('Turn', function(){
 
     beforeEach(function(){
       turn = new Turn();
+      dart = new Dart();
     });
 
   it ('should hold the number of darts thrown in the turn', function(){
     expect(turn.darts.length).toEqual(0)
+  });
+
+  it ('should allow to add darts', function(){
+    turn.addDart(dart)
+    expect(turn.darts.length).toEqual(1)
   });
 
 
--- src/turn.js
+++ src/turn.js
@@ -2,3 +2,6 @@ function Turn(){
   this.darts = []
 };
 
+Turn.prototype.addDart = function() {
+  this.darts.push(dart)
+};
